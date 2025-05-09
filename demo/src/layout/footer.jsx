92ec6c4aae6ed32581ee69db8cf4fe934cd2644c
sofiazharkikh <zharkih.sofia@yandex.ru>
2025-05-09 20:49:51 +0300

    .

---
+++ demo/src/layout/footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return <footer className="page-footer deep-purple accent-1">
+<div className="footer-copyright">
+  <div className="container">
+  © 2025 mnchllddd
+  </div>
+</div>
+</footer>
+}
+export { Footer };
